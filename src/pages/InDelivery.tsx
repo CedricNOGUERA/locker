5f7a5772d2168b68dda67648b98adc7ffb3470f8
CedricNOGUERA <cedric.noguera@lwane.com>
2023-08-16 10:42:58 -1000

    video scan layout in delivery

--- src/pages/InDelivery.tsx
+++ src/pages/InDelivery.tsx
@@ -348,11 +348,8 @@ const InDelivery: React.FC = () => {
         )}
       </Container>
       {isScan && (
-        <div className='video text-center'>
+        <div className='video-container text-center'>
           <video
-            style={{
-              width: '100%',
-            }}
             ref={videoRef}
           />
         </div>
