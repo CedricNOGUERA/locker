553d31d33e93f1908f85cd732889e6f203a0c238
CedricNOGUERA <cedric.noguera@lwane.com>
2023-10-03 14:57:41 -1000

    icon

--- src/components/ui/DeliveryDetail.tsx
+++ src/components/ui/DeliveryDetail.tsx
@@ -116,7 +116,7 @@ const DeliveryDetail = ({ scanPageProps }: any) => {
   return (
     <Container fluid className='order-list pb-5'>
       <div className='text-center'>
-        <p className='col-12 mb-0 text-center font-75'>Détail de la commande à livrer</p>
+        <p className='col-12 mb-0 text-center font-75 text-light'>Détail de la commande à livrer</p>
         <Container className='py-0 bg-gray rounded-pill shadow my-auto '>
           <Row>
             <Col
